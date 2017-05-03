56ee17e75282021be738d2107eb2e22518b768e1
Anderson Nishihara <nishihara@gmail.com>
2017-05-03 15:33:00 -0300

    Extends to list folders.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require("express"),
     skipperS3 = require('skipper-s3'),
     extend = require('extend'),
     S3Lister = require("s3-lister");
+    var knox = require('knox');
 
 //load Object Storage (S3) credentials
 var s3config = null
@@ -20,10 +21,14 @@ app.use(skipper());
 
 
 //fetch a single document from S3 storage
-app.get("/files/:filename", function (request, response) {
-    console.log(request.params.filename)
+app.get("/file", function (request, response) {
+    const file = request.query.file;
+    const path = request.query.path === '/'? '' : request.query.path;
+    const filepath = `${path}/${file}`;
+    console.log(filepath)
     var adapter = skipperS3(s3config);
-    var readStream = adapter.read(request.params.filename);
+    var readStream = adapter.read(filepath);
+    response.set({"Content-Disposition": 'attachment; filename="'+file+'"'});
     readStream.pipe(response);
 });
 
@@ -31,30 +36,46 @@ app.get("/files/:filename", function (request, response) {
 //list documents from S3 storage
 app.get("/files", function (request, response) {
     var adapter = skipperS3(s3config);
-
-    adapter.ls("/", function (error, files) {
+    const path = request.query.path || '/';
+    adapter.ls(path, function (error, files) {
         if (error) {
             console.log(error);
             response.send(error);
         }
         else {
+            console.log(files);
             response.send(files);
         }
     });
 });
+
+app.get("/folders", function (request, response) {
+ var client = knox.createClient({
+        key: s3config.key,
+        secret: s3config.secret,
+        bucket: s3config.bucket,
+        endpoint: s3config.endpoint,
+      });
+    client.list({ delimiter: '/' }, function (err, data) {
+        const folders = data.CommonPrefixes.map(prefix=>prefix.Prefix);
+        response.status(200).json(folders);
+    });
+})
+
 
 
 //upload a document to S3 storage
 app.post("/upload", function (request, response) {
     
     var file = request.file('file');
+    const path = request.query.path || '';
     var filename = file._files[0].stream.filename;
     var options = extend({}, s3config, {
         adapter: skipperS3,
         headers: {
             'x-amz-acl': 'private'
         },
-        saveAs: filename
+        saveAs: path+'/'+filename
     });
 
     file.upload(options, function (err, uploadedFiles) {
